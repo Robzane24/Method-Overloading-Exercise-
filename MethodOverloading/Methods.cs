26f3f1eb6bf908446cf16d1d3e1ca2b6b9ae2b35
Robert Zane <rob.zane24@gmail.com>
2022-12-02 11:16:08 -0600

    Completed Method Overloading

---
+++ MethodOverloading/Methods.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodOverloading
+{
+    internal class Methods
+    {
+        public static int Add(int x, int y)
+        {
+            return x + y;
+            
+        }
+
+        public static decimal Add(decimal num1, decimal num2)
+        {
+            return num1 + num2;
+        }
+
+        public static string Add(int num1, int num2, bool check)
+        {
+            var sum = num1 + num2;
+            if (check == true && sum > 1)
+            {
+                return $"{sum} dollars";
+            }
+            else if (check == true && sum == 1)
+            {
+                return $"{sum} dollars";
+            }
+            else if (check == true && sum < 1)
+            {
+                return $"{sum} dollars";
+            }
+            else
+            {
+                return sum.ToString();
+            }
+        }
+    }
+}
--- MethodOverloading/Program.cs
+++ MethodOverloading/Program.cs
@@ -1,10 +1,13 @@
-﻿namespace MethodOverloading
-{
-    public class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
-    }
-}
+﻿
+using MethodOverloading;
+
+
+
+    Console.WriteLine(Methods.Add(5, 7));
+    Console.WriteLine(Methods.Add(8.24m, 10.12m));
+    Console.WriteLine(Methods.Add(3, 4, true));
+
+    
+
+
+
